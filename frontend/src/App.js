7766f524dc612671fe7e942b09e6a4c5fdb4dc82
Sasikant Biswal <biswalsashikant@gmail.com>
2023-10-02 01:03:22 +0530

    Added homepage header and redux implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 import "./App.css";
-import {LoginPage, SignupPage, ActivationPage} from "./Routes.js";
+import {LoginPage, SignupPage, ActivationPage, HomePage} from "./Routes.js";
+import { getUser } from './redux/features/user/userSlice';
 
 const App = () => {
+  // const {} = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // dispatch(getUser());
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
         <Route path="/activation/:activation_token" element={<ActivationPage />} />
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,11 @@
 import LoginPage from "../src/pages/LoginPage.jsx";
 import SignupPage from "../src/pages/SignupPage.jsx";
 import ActivationPage from "../src/pages/ActivationPage.jsx";
+import HomePage from "../src/pages/HomePage.jsx";
 
 export {
     LoginPage,
     SignupPage,
     ActivationPage,
+    HomePage,
 }
